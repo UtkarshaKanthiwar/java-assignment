456a3b98de743dd2a12d4285757f0e1aa10bebeb
UtkarshaKanthiwar <95921742+UtkarshaKanthiwar@users.noreply.github.com>
2022-02-03 20:05:28 +0530

    Add files via upload

---
+++ Output1.java
@@ -0,0 +1,10 @@
+package project;
+
+public class Output1 {
+	public static void main(String args[]) 
+	{
+	 int arr[] = {1, 2, 3, 4, 5}; 
+	for ( int i = 0; i < arr.length - 2; ++i)
+	 System.out.println(arr[i] + " "); 
+	}
+}
