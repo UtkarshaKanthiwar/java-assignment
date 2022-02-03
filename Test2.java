72e0a6c3f45a0d63cb78e602f3ed6a474ebc09ec
UtkarshaKanthiwar <95921742+UtkarshaKanthiwar@users.noreply.github.com>
2022-02-03 20:00:19 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,10 @@
+package project;
+
+public class Test2{
+	public static void main(String[] args)
+	{
+   	 String str[] = { "Geeks", "for", "Geeks" };
+   	 for (int i = 0; i < str.length; i++)
+        System.out.print(str[i]);
+	}
+}
