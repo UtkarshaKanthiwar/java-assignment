916fc788119eb7eeeb7bb5469e51af7baee1c32d
UtkarshaKanthiwar <95921742+UtkarshaKanthiwar@users.noreply.github.com>
2022-02-13 09:25:40 -0800

    Add files via upload

---
+++ ArrayListReverseAssi.java
@@ -0,0 +1,19 @@
+package utkarsha;
+import java.util.ArrayList;
+import java.util.*;
+
+class ArrayListReverseAssi {
+
+	public static void main(String[] args) {
+		ArrayList<String>list=new ArrayList<String>();
+		list.add("Anki");
+		list.add("Niki");
+		list.add("Anki");
+		list.add("Chiya");
+		list.add("Teena");
+Collections.reverse(list);
+System.out.println(list);
+
+	}
+
+}
