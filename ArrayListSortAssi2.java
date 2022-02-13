92df11f0ec06958c32e8e855e70c33d4ab147738
UtkarshaKanthiwar <95921742+UtkarshaKanthiwar@users.noreply.github.com>
2022-02-13 09:26:07 -0800

    Add files via upload

---
+++ ArrayListSortAssi2.java
@@ -0,0 +1,17 @@
+package utkarsha;
+import java.util.ArrayList;
+import java.util.*;
+ 
+class ArrayListSortAssi2 {
+
+	public static void main(String[] args) {
+		ArrayList<String>a=new ArrayList<String>();
+				a.add("Anki");
+				a.add("Niki");
+				a.add("Pinki");
+				a.add("Chiya");
+				a.add("Teena");
+		Collections.sort(a);
+		System.out.println(a);
+			}
+	}
